b17f05a01dd07df6ecac6eba1fa788ac881c1b9f
Ben Kirwin <ben@kirw.in>
2015-03-20 15:15:11 -0400

    More useful ExampleApp
    More options, help text, direct launcher info, etc.

--- src/main/scala/samza/examples/coast/ExampleApp.scala
+++ src/main/scala/samza/examples/coast/ExampleApp.scala
@@ -23,10 +23,12 @@ import java.util.Properties
 import com.google.common.base.Charsets
 import com.google.common.io.Files
 import com.monovore.coast.model.Graph
-import com.monovore.coast.samza.ConfigGenerator
+import com.monovore.coast.samza.{SamzaConfig, ConfigGenerator}
 import com.monovore.coast.viz.Dot
 import org.apache.samza.config.Config
 import org.apache.samza.config.factories.PropertiesConfigFactory
+import org.apache.samza.job.JobRunner
+import org.apache.samza.util.Logging
 
 import scala.collection.JavaConverters._
 
@@ -36,33 +38,140 @@ import scala.collection.JavaConverters._
  *
  * If this works well, it should be moved into coast-samza directly.
  */
-abstract class ExampleApp(builder: Config => ConfigGenerator) {
+abstract class ExampleApp(builder: Config => ConfigGenerator) extends Logging {
 
   def graph: Graph
 
   def main(args: Array[String]): Unit = {
 
     args.toList match {
-      case "dot" :: Nil => {
+      case "print-dot" :: Nil => {
         println(Dot.describe(graph))
       }
-      case "dot" :: fileName :: Nil => {
+      case "save-dot" :: fileName :: Nil => {
         Files.asCharSink(new File(fileName), Charsets.UTF_8).write(Dot.describe(graph))
       }
       case "gen-config" :: basePath :: targetPath :: Nil => {
 
-        val baseConfigURI = new URI(args(0))
-        val targetDirectory = new File(args(1))
-        val configFactory = new PropertiesConfigFactory
+        val configs = withBaseConfig(basePath)
 
-        val baseConfig = configFactory.getConfig(baseConfigURI)
-        val configs = builder(baseConfig).configure(graph)
+        val targetDirectory = new File(targetPath)
 
         generateConfigFiles(targetDirectory, configs)
       }
-      case Nil => println("No arguments!")
-      case unknown => println("Unrecognized arguments: " + unknown.mkString(" "))
+      case "run" :: basePath :: Nil => {
+
+        val configs = withBaseConfig(basePath)
+
+        for ((name, config) <- configs) {
+          info(s"Launching samza job: $name")
+          new JobRunner(config).run()
+        }
+      }
+      case "run-only" :: basePath :: jobs => {
+
+        val configs = withBaseConfig(basePath).filterKeys(jobs.contains)
+
+        for ((name, config) <- configs) {
+          info(s"Launching samza job: $name")
+          new JobRunner(config).run()
+        }
+      }
+      case "info" :: basePath :: Nil => {
+
+        val ChangelogKey = "stores\\.(.+)\\.changelog".r
+        val MergeKey = "systems\\.([^\\.]+)\\.streams\\.(.+)\\.merge".r
+        val CheckpointKey = "stores\\.(.+)\\.type".r
+
+        val configs = withBaseConfig(basePath)
+
+        val CoastStream = s"[^\\.]+\\.(.+)".r
+
+        for ((name, config) <- configs) {
+
+          val pairs = config.asScala.toSeq.sortBy { _._1 }
+
+          val mergeStream = pairs
+            .collectFirst { case (MergeKey(_, stream), _) => stream }
+
+          val checkpointStream = pairs
+            .collectFirst { case (CheckpointKey(store), "checkpoint") =>
+              pairs.collectFirst { case (ChangelogKey(`store`), CoastStream(value)) => value }
+            }
+            .flatten
+
+          val changelogs = pairs
+            .collect {
+              case (ChangelogKey(store), CoastStream(value)) => store -> value
+            }
+            .filterNot { case (_, stream) => checkpointStream.exists { _ == stream } }
+
+          val inputs = config.get("task.inputs", "").split(",")
+            .filter { _.nonEmpty }
+            .collect { case CoastStream(name) => name }
+            .filterNot { stream => mergeStream.exists { _ == stream } }
+
+          println(
+            s"""
+               |$name:
+               |
+               |  input streams:
+               |    ${inputs.mkString("\n    ")}
+               |
+               |  output stream:
+               |    $name
+               |
+               |  merge stream:
+               |    ${mergeStream.getOrElse("<none>")}
+               |
+               |  checkpoint stream:
+               |    ${checkpointStream.getOrElse("<none>")}
+               |
+               |  stores:
+               |    ${changelogs.map { case (store, changelog) => s"$store:\n      changelog: $changelog" }.mkString("\n    ")}
+             """.stripMargin
+          )
+        }
+      }
+      case "help" :: Nil => printOptions()
+      case Nil => println("\nNo arguments!"); printOptions()
+      case unknown => println("\nUnrecognized arguments: " + unknown.mkString(" ")); printOptions()
     }
+  }
+
+  private[this] def printOptions(): Unit = {
+
+    println(
+      """
+        |Try one of the following:
+        |
+        |  print-dot
+        |    Prints out a representation of the job graph in GraphViz's 'dot' format.
+        |
+        |  save-dot <filename>
+        |    Writes a representation of the job graph in GraphViz's 'dot' format to the specified file.
+        |
+        |  gen-config <base config path> <output dir>
+        |    Writes the generated config for the job to the given output directory.
+        |
+        |  run <base config path>
+        |    Launches all of the Samza jobs.
+        |
+        |  run-only <base config path> [<job names>]
+        |    Launches only the specified Samza job(s).
+        |
+        |  info <base config path>
+        |    Prints a summary of inputs, outputs, and internal state.
+      """.stripMargin
+    )
+  }
+
+  private[this] def withBaseConfig(basePath: String): Map[String, Config] = {
+    val baseConfigURI = new URI(basePath)
+    val configFactory = new PropertiesConfigFactory
+    val baseConfig = configFactory.getConfig(baseConfigURI)
+
+    builder(baseConfig).configure(graph)
   }
 
   private[this] def generateConfigFiles(directory: File, configs: Map[String, Config]): Unit = {
